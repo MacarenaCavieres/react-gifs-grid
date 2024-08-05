49c1e73ed05a82d19d6342ba64ab2e102d1eb94f
MCavieres <maca.c.91@gmail.com>
2024-08-05 09:57:55 -0400

    implementacion de onclick

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,7 +1,21 @@
+import { useState } from "react";
+
 export const GifExpertApp = () => {
+    const [categories, setCategories] = useState(["Demon Slayer", "Sakura"]);
+
+    const onAddCategory = () => {
+        setCategories(["Valorant", ...categories]);
+    };
+
     return (
         <>
             <h1>GifExpertApp</h1>
+            <button onClick={onAddCategory}>Agregar</button>
+            <ol>
+                {categories.map((category) => {
+                    return <li key={category}>{category}</li>;
+                })}
+            </ol>
         </>
     );
 };
