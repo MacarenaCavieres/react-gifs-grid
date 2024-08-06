1bfb421505e6f1a91640158ebb52de6e8084b766
MCavieres <maca.c.91@gmail.com>
2024-08-06 14:16:42 -0400

    correcion de .env y useState

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,4 @@
-import "dotenv/config";
-
-const api = process.env.API_KEY;
+const api = import.meta.env.VITE_API_KEY;
 
 export const getGifs = async (category) => {
     const url = `https://api.giphy.com/v1/gifs/search?api_key=${api}=${category}&limit=20`;
