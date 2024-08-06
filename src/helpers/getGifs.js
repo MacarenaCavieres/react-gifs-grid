b99592603d856353c32184120274ffaea0085377
MCavieres <maca.c.91@gmail.com>
2024-08-06 14:05:44 -0400

    se agrega .env

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,9 @@
+import "dotenv/config";
+
+const api = process.env.API_KEY;
+
 export const getGifs = async (category) => {
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=9geVlpriNNOICGvtwW45Re3vDv5WbkI6&q=${category}&limit=20`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${api}=${category}&limit=20`;
     const resp = await fetch(url);
     const { data } = await resp.json();
     const gifs = data.map((img) => ({
